ccd7f21604768e2388e986618d3bbfff40967570
Unknown <1198995013@qq.com>
2019-07-17 16:34:29 +0800

    Add file read caching

--- src/main/java/io/github/d0048/databackend/datacore_mcml/mcmlisp/ops/FromImageFile.java
+++ src/main/java/io/github/d0048/databackend/datacore_mcml/mcmlisp/ops/FromImageFile.java
@@ -2,33 +2,41 @@ package io.github.d0048.databackend.datacore_mcml.mcmlisp.ops;
 
 import io.github.d0048.databackend.MLDataWrap;
 import io.github.d0048.databackend.datacore_mcml.mcmlisp.Molecule;
-import io.github.d0048.util.Util;
+import net.minecraft.util.math.BlockPos;
+import org.apache.commons.io.monitor.FileAlterationObserver;
 import org.apache.commons.lang3.Range;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class FromImageFile extends OPBase {
+    public static boolean doCaching = false;
+
     public FromImageFile() {
         setName("from_image");
         setNumArgs(Range.between(1, 100));
     }
 
 
+    ConcurrentHashMap<URL, MLDataWrap> pictureCacheMap = new ConcurrentHashMap<URL, MLDataWrap>();
+
     @Override
     public MLDataWrap run(List<Molecule> args) throws Exception {
         checkArguments(args);
         String urlStr = "";
         for (Molecule m : args) urlStr += m.getFullStr() + " ";
-        //System.out.println("Loading file: [" + urlStr + "]");
-        BufferedImage img = ImageIO.read(new URL(urlStr));
-        return MLDataWrap.fromBufferedImage(img);
+        URL url = new URL(urlStr);
+        if (doCaching && pictureCacheMap.keySet().contains(url)) {
+            return pictureCacheMap.get(url).clone();
+        } else {
+            //System.out.println("Loading file: [" + urlStr + "]");
+            BufferedImage img = ImageIO.read(url);
+            pictureCacheMap.put(url, MLDataWrap.fromBufferedImage(img));
+            return pictureCacheMap.get(url);
+        }
     }
 
     @Override
