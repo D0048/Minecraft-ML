cba54bae9a8a35680703a23756540dcdc730f582
Unknown <1198995013@qq.com>
2019-07-23 22:36:12 +0800

    Make data structures persistent

--- src/main/java/io/github/d0048/common/blocks/MLTensorDisplayTileEntity.java
+++ src/main/java/io/github/d0048/common/blocks/MLTensorDisplayTileEntity.java
@@ -5,6 +5,7 @@ import io.github.d0048.MLConfig;
 import io.github.d0048.common.MLAsyncHelper;
 import io.github.d0048.databackend.MLDataWrap;
 import io.github.d0048.util.ParticleUtil;
+import io.github.d0048.util.SerializableBlockPos;
 import io.github.d0048.util.Util;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
@@ -13,6 +14,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import org.apache.commons.lang3.Range;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,8 +24,8 @@ public class MLTensorDisplayTileEntity extends MLTileEntityBase {
     String dataID = "";
     int[] displayShape = new int[]{1, 1, 1};
     BlockPos edgeLow = new BlockPos(0, 0, 0), edgeHigh = edgeLow;
-    ConcurrentHashMap<BlockPos, Integer> pos2IndexMap = new ConcurrentHashMap<BlockPos, Integer>();
-    ConcurrentHashMap<Integer, BlockPos> index2PosMap = new ConcurrentHashMap<Integer, BlockPos>();
+    ConcurrentHashMap<SerializableBlockPos, Integer> pos2IndexMap = new ConcurrentHashMap<>();
+    ConcurrentHashMap<Integer, SerializableBlockPos> index2PosMap = new ConcurrentHashMap<>();
     Range<Double> normalizationRange = Range.between(-1D, 1D);
 
     public MLTensorDisplayTileEntity() {
@@ -67,8 +70,8 @@ public class MLTensorDisplayTileEntity extends MLTileEntityBase {
             Cleanup().unregisterID();
             this.dataID = dataID.trim();
             if ((MCML.mlDataCore.registerDataForID(dataID)) != null) {
-                if (Util.arrCumProduct(displayShape) >= Util.arrCumProduct(getDataWrap().getShape()))
-                    setDisplayShape(getDataWrap().getShape().clone());
+                //if (Util.arrCumProduct(displayShape) >= Util.arrCumProduct(getDataWrap().getShape()))
+                //    setDisplayShape(getDataWrap().getShape().clone());
                 solveDataWrap();
                 return true;
             }
@@ -88,6 +91,199 @@ public class MLTensorDisplayTileEntity extends MLTileEntityBase {
         reDraw();
         markDirty();
         return true;
+    }
+
+
+    public void writeValues() {
+        if (getWorld() == null || getDataWrap() == null) return;
+        //info("writing: " + index2PosMap);
+        Set<Integer> indexs = index2PosMap.keySet();
+        if (indexs.isEmpty()) {
+            solveDataWrap();
+            return;
+        }
+        double[] values = getDataWrap().getData();
+        double min = getNormalizationRange().getMinimum(), max = getNormalizationRange().getMaximum();
+
+
+        for (int i : indexs) {
+            double value = values[i];
+            int normedValue = (int) ((Math.min(max, Math.max(min, value)) - min) / (max - min) * MLConfig.scalarResolution);
+            MLScalar.placeAt(getWorld(), index2PosMap.get(i).getPos(), normedValue);
+            //info("write: " + index2PosMap.get(i).getPos());
+        }
+        MCML.mlDataCore.writeDataForID(getDataID());
+    }
+
+    public void readValues() {
+        if (getWorld() == null || getDataWrap() == null) return;
+        //info("reading: " + pos2IndexMap);
+        Set<SerializableBlockPos> indexs = pos2IndexMap.keySet();
+        double[] values = getDataWrap().getData();
+        double min = getNormalizationRange().getMinimum(), max = getNormalizationRange().getMaximum();
+
+        for (SerializableBlockPos p : indexs) {
+            double value = MLScalar.getValueAt(getWorld(), p.getPos());
+            double deNormedValue = value / MLConfig.scalarResolution * (max - min) + min;
+            values[pos2IndexMap.get(p)] = deNormedValue;
+            //info("read: " + p.getPos());
+        }
+    }
+
+    MLTensorDisplayTileEntity Cleanup() {
+        if (getWorld() != null)
+            for (int i = edgeLow.getX(); i <= edgeHigh.getX() - 1; i++) {
+                for (int j = edgeLow.getY(); j <= edgeHigh.getY() - 1; j++) {
+                    for (int k = edgeLow.getZ(); k <= edgeHigh.getZ() - 1; k++) {
+                        world.setBlockState(new BlockPos(i, j, k), Blocks.AIR.getDefaultState());
+                    }
+                }
+            }
+        pos2IndexMap.clear();
+        index2PosMap.clear();
+        return this;
+    }
+
+    MLTensorDisplayTileEntity unregisterID() {
+        MCML.mlDataCore.unregisterID(getDataID());
+        return this;
+    }
+
+    public static Thread dataSolver = new Thread();
+
+    MLTensorDisplayTileEntity solveDataWrap() {
+        if (getDataWrap() == null || getWorld() == null) return this;
+
+        int[] shape = getDisplayShape();
+        if (shape.length > 3)
+            MCML.logger.warn("Displaying a tensor larger than 3D, only first 3 used!");
+
+        edgeHigh = edgeLow.add(shape[0], shape[1], shape[2]);
+        //info("Solving Data: " + Arrays.toString(data) + " with shape " + Arrays.toString(shape));
+        info("Solving Data: [" + getDataID() + "] with shape " + Arrays.toString(shape));
+        final int[] shapeFinal = shape.clone();
+        dataSolver.interrupt();
+        (dataSolver = new Thread(() -> {
+            info("Staring a new thread to solve datawrap");
+            for (int i = 0; i < shapeFinal[0] || i == 0; i++)
+                for (int j = 0; j < shapeFinal[1] || j == 0; j++) {
+                    for (int k = 0; k < shapeFinal[2] || k == 0; k++) {
+                        BlockPos p = edgeHigh.add(-i - 1, -j - 1, -k - 1);
+                        try {
+                            int index = getDataWrap().extIndex2InternalIndex(new int[]{i, j, k});
+                            pos2IndexMap.put(new SerializableBlockPos(p), index);
+                            index2PosMap.put(index, new SerializableBlockPos(p));
+                            //MLScalar.placeAt(getWorld(), p, val);
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }
+            info("Datawrap sovled.");
+        })).start();
+        if (isWritable())
+            readValues();
+        else writeValues();
+        return this;
+    }
+
+    boolean isDisplayShapeValid() {
+        if (getDataWrap() != null)
+            return Util.arrCumProduct(getDisplayShape()) <= Util.arrCumProduct(getDataWrap().getShape());
+        else return false;
+    }
+
+
+    @Override
+    public String toString() {
+        String ret = TextFormatting.LIGHT_PURPLE + super.toString() + ":\n";
+        ret += TextFormatting.LIGHT_PURPLE + "    - Mode: " + TextFormatting.YELLOW + (isWritable() ? "Read/Write" : "Read Only")
+                + TextFormatting.LIGHT_PURPLE + "\n";
+        ret += TextFormatting.LIGHT_PURPLE + "    - DataID: " + TextFormatting.YELLOW + getDataID()
+                + TextFormatting.LIGHT_PURPLE + "\n";
+        ret += TextFormatting.LIGHT_PURPLE + "    - DataWrap: " + getDataWrap() + "\n";
+        ret += TextFormatting.LIGHT_PURPLE + "    - Display Shape: " + TextFormatting.YELLOW
+                + (getDataWrap() == null ? -1 : getDataWrap().getData().length) + TextFormatting.LIGHT_PURPLE + " reshaped into "
+                + TextFormatting.YELLOW + Arrays.toString(displayShape) + " | " + getNormalizationRange() +
+                TextFormatting.LIGHT_PURPLE + " which is " + TextFormatting.YELLOW + (isDisplayShapeValid() ? "valid" : "invalid") +
+                TextFormatting.YELLOW + "\n";
+        ret += TextFormatting.LIGHT_PURPLE + "    - Goes from: " + TextFormatting.YELLOW + edgeLow + " to " + edgeHigh;
+        return ret;
+
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        compound.setString("dataID", dataID);
+        compound.setIntArray("myPosition", new int[]{getPos().getX(), getPos().getY(), getPos().getZ()});
+        compound.setIntArray("edgeHigh", new int[]{edgeHigh.getX(), edgeHigh.getY(), edgeHigh.getZ()});
+        compound.setIntArray("edgeLow", new int[]{edgeLow.getX(), edgeLow.getY(), edgeLow.getZ()});
+        compound.setIntArray("displayShape", displayShape);
+        compound.setDouble("rangeMax", getNormalizationRange().getMaximum());
+        compound.setDouble("rangeMin", getNormalizationRange().getMinimum());
+        compound.setBoolean("writable", isWritable());
+        try {
+            compound.setByteArray("serialize_index2PosMap", Util.serialize(index2PosMap));
+            compound.setByteArray("serialize_pos2IndexMap", Util.serialize(pos2IndexMap));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        markDirty();
+        return compound;
+
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        if (compound.hasKey("edgeHigh"))
+            edgeHigh = new BlockPos(compound.getIntArray("edgeHigh")[0], compound.getIntArray("edgeHigh")[1],
+                    compound.getIntArray("edgeHigh")[2]);
+        if (compound.hasKey("edgeLow"))
+            edgeLow = new BlockPos(compound.getIntArray("edgeLow")[0], compound.getIntArray("edgeLow")[1],
+                    compound.getIntArray("edgeLow")[2]);
+        if (compound.hasKey("displayShape")) {
+            setDisplayShape(compound.getIntArray("displayShape"));
+        }
+        if (compound.hasKey("writable")) {
+            try {
+                setWritable(compound.getBoolean("writable"));
+            } catch (Exception e) {//meant to fail on load
+            }
+        }
+        try {
+            index2PosMap = (ConcurrentHashMap<Integer, SerializableBlockPos>)
+                    Util.deserialize(compound.getByteArray("serialize_index2PosMap"));
+            pos2IndexMap = (ConcurrentHashMap<SerializableBlockPos, Integer>)
+                    Util.deserialize(compound.getByteArray("serialize_pos2IndexMap"));
+            if (compound.getBoolean("writable"))
+                readValues();
+            else
+                writeValues();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (compound.hasKey("dataID")) {
+            try {
+                //setDataID(compound.getString("dataID"));
+                this.dataID = compound.getString("dataID");
+                MCML.mlDataCore.registerDataForID(dataID);
+            } catch (Exception e) {
+                info("Malformed DataID: " + compound.getString("dataID"));
+                MCML.logger.error(e);
+            }
+        }
+
+        double max = 1, min = -1;
+        if (compound.hasKey("rangeMax")) {
+            max = (compound.getDouble("rangeMax"));
+        }
+        if (compound.hasKey("rangeMin")) {
+            min = (compound.getDouble("rangeMin"));
+        }
+        setNormalizationRange(Range.between(min, max));
+
     }
 
     public MLTensorDisplayTileEntity normalize() {
@@ -130,188 +326,6 @@ public class MLTensorDisplayTileEntity extends MLTileEntityBase {
     public MLTensorDisplayTileEntity reDraw() {
         Cleanup().solveDataWrap();
         return this;
-    }
-
-    public void writeValues() {
-        if (getWorld() == null || getDataWrap() == null) return;
-        Set<Integer> indexs = index2PosMap.keySet();
-        if (indexs.isEmpty()) {
-            solveDataWrap();
-            return;
-        }
-        double[] values = getDataWrap().getData();
-        double min = getNormalizationRange().getMinimum(), max = getNormalizationRange().getMaximum();
-
-
-        for (int i : indexs) {
-            double value = values[i];
-            int normedValue = (int) ((Math.min(max, Math.max(min, value)) - min) / (max - min) * MLConfig.scalarResolution);
-            MLScalar.placeAt(getWorld(), index2PosMap.get(i), normedValue);
-        }
-        MCML.mlDataCore.writeDataForID(getDataID());
-    }
-
-    public void readValues() {
-        if (getWorld() == null || getDataWrap() == null) return;
-        Set<BlockPos> indexs = pos2IndexMap.keySet();
-        double[] values = getDataWrap().getData();
-        double min = getNormalizationRange().getMinimum(), max = getNormalizationRange().getMaximum();
-
-        for (BlockPos p : indexs) {
-            double value = MLScalar.getValueAt(getWorld(), p);
-            double deNormedValue = value / MLConfig.scalarResolution * (max - min) + min;
-            values[pos2IndexMap.get(p)] = deNormedValue;
-        }
-    }
-
-    MLTensorDisplayTileEntity Cleanup() {
-        if (getWorld() != null)
-            for (int i = edgeLow.getX(); i <= edgeHigh.getX() - 1; i++) {
-                for (int j = edgeLow.getY(); j <= edgeHigh.getY() - 1; j++) {
-                    for (int k = edgeLow.getZ(); k <= edgeHigh.getZ() - 1; k++) {
-                        world.setBlockState(new BlockPos(i, j, k), Blocks.AIR.getDefaultState());
-                        //getWorld().destroyBlock(new BlockPos(i, j, k), false);
-                        //MLAsyncHelper.removeAsync(getWorld(), new BlockPos(i, j, k));
-                    }
-                }
-            }
-        pos2IndexMap.clear();
-        index2PosMap.clear();
-        //if (isDisplayShapeValid())
-        //edgeHigh = edgeLow.add(getDisplayShape()[0], getDisplayShape()[1], getDisplayShape()[2]);
-        //else {
-        //    edgeLow = edgeHigh = this.getPos().add(1, 0, 1);
-        //}
-        return this;
-    }
-
-    MLTensorDisplayTileEntity unregisterID() {
-        MCML.mlDataCore.unregisterID(getDataID());
-        return this;
-    }
-
-    public static Thread dataSolver = new Thread();
-
-    MLTensorDisplayTileEntity solveDataWrap() {
-        if (getDataWrap() == null || getWorld() == null) return this;
-
-        int[] shape = getDisplayShape();
-        /*
-        if (!isDisplayShapeValid()) {
-            MCML.logger.warn("Display shape too large, use original instead!");
-            shape = getDataWrap().getShape();
-            while (shape.length < 3) { //pad to 3D
-                shape = Arrays.copyOf(shape, shape.length + 1);
-                shape[shape.length - 1] = 1;
-            }
-        }*/
-        if (shape.length > 3)
-            MCML.logger.warn("Displaying a tensor larger than 3D, only first 3 used!");
-
-        edgeHigh = edgeLow.add(shape[0], shape[1], shape[2]);
-        //info("Solving Data: " + Arrays.toString(data) + " with shape " + Arrays.toString(shape));
-        info("Solving Data: [" + getDataID() + "] with shape " + Arrays.toString(shape));
-        final int[] shapeFinal = shape.clone();
-        dataSolver.interrupt();
-        (dataSolver = new Thread(() -> {
-            info("Staring a new thread to solve datawrap");
-            for (int i = 0; i < shapeFinal[0] || i == 0; i++)
-                for (int j = 0; j < shapeFinal[1] || j == 0; j++) {
-                    for (int k = 0; k < shapeFinal[2] || k == 0; k++) {
-                        BlockPos p = edgeHigh.add(-i - 1, -j - 1, -k - 1);
-                        try {
-                            int index = getDataWrap().extIndex2InternalIndex(new int[]{i, j, k});
-                            pos2IndexMap.put(p, index);
-                            index2PosMap.put(index, p);
-                            //MLScalar.placeAt(getWorld(), p, val);
-                        } catch (Exception e) {
-
-                        }
-                    }
-                }
-            info("Datawrap sovled.");
-        })).start();
-        if (isWritable())
-            readValues();
-        else writeValues();
-        return this;
-    }
-
-    boolean isDisplayShapeValid() {
-        if (getDataWrap() != null)
-            return Util.arrCumProduct(getDisplayShape()) <= Util.arrCumProduct(getDataWrap().getShape());
-        else return false;
-    }
-
-    @Override
-    public String toString() {
-        String ret = TextFormatting.LIGHT_PURPLE + super.toString() + ":\n";
-        ret += TextFormatting.LIGHT_PURPLE + "    - Mode: " + TextFormatting.YELLOW + (isWritable() ? "Read/Write" : "Read Only")
-                + TextFormatting.LIGHT_PURPLE + "\n";
-        ret += TextFormatting.LIGHT_PURPLE + "    - DataID: " + TextFormatting.YELLOW + getDataID()
-                + TextFormatting.LIGHT_PURPLE + "\n";
-        ret += TextFormatting.LIGHT_PURPLE + "    - DataWrap: " + getDataWrap() + "\n";
-        ret += TextFormatting.LIGHT_PURPLE + "    - Display Shape: " + TextFormatting.YELLOW
-                + (getDataWrap() == null ? -1 : getDataWrap().getData().length) + TextFormatting.LIGHT_PURPLE + " reshaped into "
-                + TextFormatting.YELLOW + Arrays.toString(displayShape) + " | " + getNormalizationRange() +
-                TextFormatting.LIGHT_PURPLE + " which is " + TextFormatting.YELLOW + (isDisplayShapeValid() ? "valid" : "invalid") +
-                TextFormatting.YELLOW + "\n";
-        return ret;
-
-    }
-
-    @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        super.writeToNBT(compound);
-        compound.setString("dataID", dataID);
-        compound.setIntArray("myPosition", new int[]{getPos().getX(), getPos().getY(), getPos().getZ()});
-        compound.setIntArray("edgeHigh", new int[]{edgeHigh.getX(), edgeHigh.getY(), edgeHigh.getZ()});
-        compound.setIntArray("edgeLow", new int[]{edgeLow.getX(), edgeLow.getY(), edgeLow.getZ()});
-        compound.setIntArray("displayShape", displayShape);
-        compound.setDouble("rangeMax", getNormalizationRange().getMaximum());
-        compound.setDouble("rangeMin", getNormalizationRange().getMinimum());
-        compound.setBoolean("writable", isWritable());
-        markDirty();
-        return compound;
-
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound compound) {
-        super.readFromNBT(compound);
-        if (compound.hasKey("edgeHigh"))
-            edgeHigh = new BlockPos(compound.getIntArray("edgeHigh")[0], compound.getIntArray("edgeHigh")[1],
-                    compound.getIntArray("edgeHigh")[2]);
-        if (compound.hasKey("edgeLow"))
-            edgeLow = new BlockPos(compound.getIntArray("edgeLow")[0], compound.getIntArray("edgeLow")[1],
-                    compound.getIntArray("edgeLow")[2]);
-        if (compound.hasKey("displayShape")) {
-            setDisplayShape(compound.getIntArray("displayShape"));
-        }
-        if (compound.hasKey("writable")) {
-            try {
-                setWritable(compound.getBoolean("writable"));
-            } catch (Exception e) {//meant to fail on load
-            }
-        }
-        if (compound.hasKey("dataID")) {
-            try {
-                setDataID(compound.getString("dataID"));
-            } catch (Exception e) {
-                info("Malformed DataID: " + compound.getString("dataID"));
-                MCML.logger.error(e);
-            }
-        }
-
-        double max = 1, min = -1;
-        if (compound.hasKey("rangeMax")) {
-            max = (compound.getDouble("rangeMax"));
-        }
-        if (compound.hasKey("rangeMin")) {
-            min = (compound.getDouble("rangeMin"));
-        }
-        setNormalizationRange(Range.between(min, max));
-
     }
 
     public MLTensorDisplayTileEntity setWritable(boolean b) {
